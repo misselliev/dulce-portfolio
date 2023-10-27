7cb28c52373626ace8f2818004c3345affc58f32
Elizabeth Villalejos <elizabeth.villalejos@gmail.com>
2023-10-26 17:55:24 -0700

    updating projects with carousel

--- src/components/ProjectCard2.jsx
+++ src/components/ProjectCard2.jsx
@@ -2,15 +2,12 @@
 import Card from "react-bootstrap/Card";
 import ListGroup from "react-bootstrap/ListGroup";
 function ProjectCard(project) {
-  // console.log(project);
   const { title, image, subtitle, stack, link, live } = project.project;
-  console.log(project);
   return (
     <Card>
       <Card.Header as="h5">{title}</Card.Header>
       <Card.Img variant="top" src={image} />
       <Card.Body>
-        {/* <Card.Title>Card Title</Card.Title> */}
         <Card.Text>{subtitle}</Card.Text>
       </Card.Body>
       <ListGroup className="list-group-flush">
--- src/components/ProjectSection.jsx
+++ src/components/ProjectSection.jsx
@@ -1,21 +1,41 @@
 import Col from "react-bootstrap/Col";
-import Row from "react-bootstrap/Row";
 import { Container } from "react-bootstrap";
 import ProjectCard from "./ProjectCard2";
 import projectsData from "../assets/data/projects";
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
+
 function ProjectSection() {
+  const responsive = {
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 2,
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 1,
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1,
+    },
+  };
+
   return (
     <section className="project" id="projects">
       <h2>Projects</h2>
       <Container>
-        <Row xs={1} md={2} lg={3} className="g-4">
-          {console.log(projectsData)}
+        <Carousel
+          responsive={responsive}
+          infinite={true}
+          className="owl-carousel owl-theme project-slider"
+        >
           {projectsData.map((item, idx) => (
             <Col key={idx}>
               <ProjectCard project={item} />
             </Col>
           ))}
-        </Row>
+        </Carousel>
       </Container>
     </section>
   );

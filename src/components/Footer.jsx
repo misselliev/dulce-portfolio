7df460f3c36127d4422de7f114e143912c558cbf
Elizabeth Villalejos <elizabeth.villalejos@gmail.com>
2023-10-26 17:15:03 -0700

    updating footer and navbar

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,30 +1,38 @@
 import { Container, Row, Col } from "react-bootstrap";
-import logo from "../assets/img/logo.svg";
 import navIcon1 from "../assets/img/nav-icon1.svg";
-import navIcon2 from "../assets/img/nav-icon2.svg";
-import navIcon3 from "../assets/img/nav-icon3.svg";
+import githubWhite from "../assets/img/githubWhite.svg";
+import mailIcon from "../assets/img/mailIcon.png";
+// import navIcon3 from "../assets/img/nav-icon3.svg";
 
 export const Footer = () => {
   return (
     <footer className="footer">
       <Container>
         <Row className="align-items-center">
-          <Col size={12} sm={6}>
+          {/* <Col size={12} sm={6}>
             <img src={logo} alt="Logo" />
-          </Col>
-          <Col size={12} sm={6} className="text-center text-sm-end">
+          </Col> */}
+          <Col size={12} className="text-center">
             <div className="social-icon">
-              <a href="#">
-                <img src={navIcon1} alt="Icon" />
+              <a
+                href="https://www.linkedin.com/in/elivillalejos/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img src={navIcon1} alt="Linkedin Icon" />
               </a>
-              <a href="#">
-                <img src={navIcon2} alt="Icon" />
+              <a
+                href="https://github.com/misselliev"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img src={githubWhite} alt="Github Icon" />
               </a>
-              <a href="#">
-                <img src={navIcon3} alt="Icon" />
+              <a href="mailto:elizabeth.villalejos@gmail.com?subject=Lets%20Connect">
+                <img src={mailIcon} alt="Compose Email Icon" />
               </a>
             </div>
-            <p>Copyright 2022. All Rights Reserved</p>
+            <p>Copyright 2023. All Rights Reserved</p>
           </Col>
         </Row>
       </Container>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,9 @@
 import { useState, useEffect } from "react";
 import { Navbar, Nav, Container } from "react-bootstrap";
-import logo from "../assets/img/logo.svg";
 import navIcon1 from "../assets/img/nav-icon1.svg";
-import navIcon2 from "../assets/img/nav-icon2.svg";
-import navIcon3 from "../assets/img/nav-icon3.svg";
-import { HashLink } from "react-router-hash-link";
+// import navIcon2 from "../assets/img/nav-icon2.svg";
+import mailIcon from "../assets/img/mailIcon.png";
+import githubWhite from "../assets/img/githubWhite.svg";
 import { BrowserRouter as Router } from "react-router-dom";
 
 export const NavBar = () => {
@@ -33,9 +32,9 @@ export const NavBar = () => {
     <Router>
       <Navbar expand="md" className={scrolled ? "scrolled" : ""}>
         <Container>
-          <Navbar.Brand href="/">
+          {/* <Navbar.Brand href="/">
             <img src={logo} alt="Logo" />
-          </Navbar.Brand>
+          </Navbar.Brand> */}
           <Navbar.Toggle aria-controls="basic-navbar-nav">
             <span className="navbar-toggler-icon"></span>
           </Navbar.Toggle>
@@ -73,21 +72,29 @@ export const NavBar = () => {
             </Nav>
             <span className="navbar-text">
               <div className="social-icon">
-                <a href="#">
-                  <img src={navIcon1} alt="" />
+                <a
+                  href="https://www.linkedin.com/in/elivillalejos/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <img src={navIcon1} alt="Linkedin Icon" />
                 </a>
-                <a href="#">
-                  <img src={navIcon2} alt="" />
+                <a
+                  href="https://github.com/misselliev"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <img className="github" src={githubWhite} alt="Github Icon" />
                 </a>
-                <a href="#">
-                  <img src={navIcon3} alt="" />
+                <a href="mailto:elizabeth.villalejos@gmail.com?subject=Lets%20Connect">
+                  <img src={mailIcon} alt="Compose Email Icon" />
                 </a>
               </div>
-              <HashLink to="#connect">
+              {/* <HashLink to="#connect">
                 <button className="vvd">
                   <span>Let’s Connect</span>
                 </button>
-              </HashLink>
+              </HashLink> */}
             </span>
           </Navbar.Collapse>
         </Container>

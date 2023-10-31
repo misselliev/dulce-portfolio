fde20eb0ab5e7a05d751d4d514c7da293113b54f
Elizabeth Villalejos <elizabeth.villalejos@gmail.com>
2023-10-31 15:24:57 -0700

    updating footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,16 +2,14 @@ import { Container, Row, Col } from "react-bootstrap";
 import navIcon1 from "../assets/img/nav-icon1.svg";
 import githubWhite from "../assets/img/githubWhite.svg";
 import mailIcon from "../assets/img/mailIcon.png";
-// import navIcon3 from "../assets/img/nav-icon3.svg";
 
 export const Footer = () => {
+  const year = new Date().getFullYear();
+
   return (
     <footer className="footer">
       <Container>
         <Row className="align-items-center">
-          {/* <Col size={12} sm={6}>
-            <img src={logo} alt="Logo" />
-          </Col> */}
           <Col size={12} className="text-center">
             <div className="social-icon">
               <a
@@ -32,7 +30,23 @@ export const Footer = () => {
                 <img src={mailIcon} alt="Compose Email Icon" />
               </a>
             </div>
-            <p>Copyright 2023. All Rights Reserved</p>
+            <p>
+              Built with
+              <svg
+                fill="#e53e3e"
+                viewBox="0 0 24 24"
+                className="w-5 h-5 mx-1 pt-px text-red-600"
+                stroke="e53e3e"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
+                />
+              </svg>
+              by Elizabeth Villalejos© {year}.
+            </p>
           </Col>
         </Row>
       </Container>
